f9164dc6b2490b06ea2552fe20e035108043920c
Alejandro Ferrer-Peasley <36723243+alexferrer17@users.noreply.github.com>
2019-04-26 10:49:13 -0700

    Create cars.sh

---
+++ cars.sh
@@ -0,0 +1,23 @@
+word = ""
+
+while [ "$word" != "quit" ]
+do 
+        echo "Would you like to add a car(1), List the cars in the invetory(2) or quit(quit)"
+        read word
+        if [ "$word" = "1" ]
+        then
+                echo "what year?"
+                read year
+                echo "what make?"
+                read make
+                echo "what model?"
+                read model
+                car = "$year:$make:$model"
+                car >> My_old_cars
+        else 
+                so       
+
+        fi
+done
+echo "Goodbye"
+  
